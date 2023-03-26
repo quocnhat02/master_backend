510d42fef4ae07bcca3229f61653c1a4907ccc32
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2023-03-26 14:07:32 +0700

    props 1

--- src/App.js
+++ src/App.js
@@ -8,14 +8,13 @@ function YoutubeItem(props) {
       </div>
       <div className='youtube-footer'>
         <div className='youtube-avatar'>
-          <img
-            src='https://res.cloudinary.com/practicaldev/image/fetch/s--4h1_TJud--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/zd6p2ia2bn6ba6t8j11o.png'
-            width='50px'
-          />
+          <img src={props.avatar} width='50px' />
         </div>
         <div className='youtube-info'>
-          <h3 className='youtube-heading'>Title</h3>
-          <h4 className='youtube-author'>{props.author}</h4>
+          <h3 className='youtube-heading'>
+            {props.title || 'This is example of title'}
+          </h3>
+          <h4 className='youtube-author'>{props.author || 'Anonymous'}</h4>
         </div>
       </div>
     </div>
@@ -27,6 +26,8 @@ function App() {
     <div>
       <YoutubeItem
         image='https://res.cloudinary.com/practicaldev/image/fetch/s--4h1_TJud--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/zd6p2ia2bn6ba6t8j11o.png'
+        avatar='https://m.timesofindia.com/photo/83890825/83890825.jpg'
+        title='Learn React'
         author='NhatDev'
       />
     </div>
