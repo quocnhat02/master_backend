b8cfc128ac94257357c381bd3434b5718dc65a6c
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2023-03-26 13:39:58 +0700

    feature example

--- src/App.js
+++ src/App.js
@@ -3,21 +3,10 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='feature'>
+      <img className='feature-image' />
+      <h3 className='feature-title'>Title</h3>
+      <p className='feature-desc'>Description</p>
     </div>
   );
 }
