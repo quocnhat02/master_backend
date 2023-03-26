bb95c02a3732acd3f933ea4eac871c91f7b1ae9d
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2023-03-26 19:18:01 +0700

    stateless and stateful

---
+++ src/components/state/Toggle.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// stateless functional component: component nhung khong su dung state
+const Toggle = () => {
+  return <div>Toggle</div>;
+};
+
+// stateful functional component: component co su dung state
+const Toggle2 = () => {
+  //   const [count, setCount] = useState();
+  return <div>Toggle</div>;
+};
+
+export default Toggle;
