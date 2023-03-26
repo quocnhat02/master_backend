5c7378c86d74c8e52be62db61e0b2e041798148d
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2023-03-26 13:47:12 +0700

    feature component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function Feature() {
   return (
     <div className='feature'>
       <img className='feature-image' />
       <h3 className='feature-title'>Title</h3>
       <p className='feature-desc'>Description</p>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <Feature />
+      <Feature />
+      <Feature />
     </div>
   );
 }
