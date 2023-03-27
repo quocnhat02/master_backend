59850d6a161b10660d80d6c6de21ca4b27b6b149
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2023-03-27 14:08:27 +0700

    useState 2

--- src/components/state/Toggle.jsx
+++ src/components/state/Toggle.jsx
@@ -4,19 +4,21 @@ import './ToggleStyles.css';
 const Toggle = () => {
   const [on, setOn] = useState(false);
 
+  const handleToggle = () => setOn((on) => !on);
+
   return (
     <>
-      <div className={`toggle ${on ? 'active' : ''}`}>
+      <div className={`toggle ${on ? 'active' : ''}`} onClick={handleToggle}>
         <div className={`spinner ${on ? 'active' : ''}`}></div>
       </div>
-      <div className='toggle-control'>
+      {/* <div className='toggle-control'>
         <div className='toggle-on' onClick={() => setOn(true)}>
           On
         </div>
         <div className='toggle-off' onClick={() => setOn(false)}>
           Off
         </div>
-      </div>
+      </div> */}
     </>
   );
 };
