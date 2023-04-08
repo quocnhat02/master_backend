a2e3c693e28f64ccea87f31f93c14af482901b59
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2023-04-08 23:56:40 +0700

    styled component 1

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -2,27 +2,54 @@
 import React from 'react';
 import styled from 'styled-components';
 
+const StyledCard = styled.div`
+  position: relative;
+`;
+const CardImage = styled.div`
+  height: 400px;
+  width: 400px;
+  border-radius: 8px;
+`;
+const CardImg = styled.img`
+  display: block;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  border-radius: inherit;
+`;
+const CardContent = styled.div`
+  position: absolute;
+  left: 50%;
+  transform: translate(-50%, 0);
+  bottom: 0;
+  background-color: white;
+  z-index: 10;
+  border-radius: 20px;
+`;
+
 const Card = () => {
   return (
-    <div>
-      <img src='https://wallpaperaccess.com/full/54671.jpg' />
-      <div className=''>
-        <div className=''>
-          <div className=''>
+    <StyledCard>
+      <CardImage>
+        <CardImg src='https://wallpaperaccess.com/full/54671.jpg' />
+      </CardImage>
+      <CardContent>
+        <div>
+          <div>
             <img
               src='https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8YXZhdGFyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60'
               alt=''
             />
             <span>@nhatnguyen</span>
           </div>
-          <div className=''>256</div>
+          <div>256</div>
         </div>
-        <div className=''>
+        <div>
           <h3>ReactJS</h3>
           <span>12,000 VND</span>
         </div>
-      </div>
-    </div>
+      </CardContent>
+    </StyledCard>
   );
 };
 
