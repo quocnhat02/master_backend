a80f9cdeb30596221c0ea0684c37e733d1c22651
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2023-04-09 00:27:44 +0700

    styled component 2

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ function App() {
   return (
     <div>
       <Card />
+      <Card />
+      <Card />
+      <Card />
     </div>
   );
 }
--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -4,10 +4,11 @@ import styled from 'styled-components';
 
 const StyledCard = styled.div`
   position: relative;
+  width: 400px;
 `;
 const CardImage = styled.div`
   height: 400px;
-  width: 400px;
+  width: 100%;
   border-radius: 8px;
 `;
 const CardImg = styled.img`
@@ -20,11 +21,59 @@ const CardImg = styled.img`
 const CardContent = styled.div`
   position: absolute;
   left: 50%;
-  transform: translate(-50%, 0);
+  transform: translate(-50%, 50%);
+  width: calc(100% - 36px);
   bottom: 0;
   background-color: white;
   z-index: 10;
   border-radius: 20px;
+  padding: 20px;
+`;
+const CardTop = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 30px;
+`;
+const CardUser = styled.div`
+  display: flex;
+  align-items: center;
+  column-gap: 12px;
+`;
+const UserAvatar = styled.img`
+  width: 30px;
+  height: 30px;
+  border-radius: 100rem;
+  object-fit: cover;
+  flex-shrink: 0;
+`;
+const UserName = styled.span`
+  font-weight: 300;
+  font-size: 16px;
+  color: #333;
+`;
+const CardFooter = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+const CardTitle = styled.h3`
+  font-size: 18px;
+  font-weight: 500;
+  color: black;
+`;
+const CardAmount = styled.span`
+  font-size: 18px;
+  font-weight: bold;
+  background: linear-gradient(
+    90deg,
+    rgba(244, 86, 232, 1) 0%,
+    rgba(154, 253, 29, 1) 85%,
+    rgba(69, 252, 198, 1) 100%
+  );
+  color: transparent;
+  -webkit-background-clip: text;
+  background-clip: text;
 `;
 
 const Card = () => {
@@ -34,20 +83,20 @@ const Card = () => {
         <CardImg src='https://wallpaperaccess.com/full/54671.jpg' />
       </CardImage>
       <CardContent>
-        <div>
-          <div>
-            <img
+        <CardTop>
+          <CardUser>
+            <UserAvatar
               src='https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8YXZhdGFyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60'
               alt=''
             />
-            <span>@nhatnguyen</span>
-          </div>
+            <UserName>@nhatnguyen</UserName>
+          </CardUser>
           <div>256</div>
-        </div>
-        <div>
-          <h3>ReactJS</h3>
-          <span>12,000 VND</span>
-        </div>
+        </CardTop>
+        <CardFooter>
+          <CardTitle>ReactJS</CardTitle>
+          <CardAmount>12,000 VND</CardAmount>
+        </CardFooter>
       </CardContent>
     </StyledCard>
   );
