1fa4a726163d4abe64bb3bedf956e935a852028a
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2023-04-09 14:34:54 +0700

    useEffect 2

--- src/components/photo/Photos.jsx
+++ src/components/photo/Photos.jsx
@@ -1,10 +1,24 @@
 import React, { useEffect } from 'react';
+import axios from 'axios';
+
+// https://piscum.photos/v2/list
+
+const GetPhotos = () => {
+  return axios
+    .get('https://piscum.photos/v2/list')
+    .then((res) => {
+      console.log(res);
+    })
+    .catch((error) => console.log(error));
+};
 
 const Photos = () => {
   // useEffect(function callback() {
   //     // side-effect
   // }, []);
-  useEffect(() => {}, []);
+  useEffect(() => {
+    document.title = 'Welcome to useEffect';
+  }, []);
 
   return <div>Photos</div>;
 };
