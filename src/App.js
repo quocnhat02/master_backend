fd1854236fefe5b497dc8ae0141e7808051b152b
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2023-04-09 14:24:31 +0700

    useEffect 1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.scss';
+import Photos from './components/photo/Photos';
 // import { GlobalStyles } from './GlobalStyles';
 // import Card from './components/card/Card';
 // import Card2 from './components/card/Card2';
-import CardList from './components/card/CardList';
-import CardTailwind from './components/card/CardTailwind';
+// import CardList from './components/card/CardList';
+// import CardTailwind from './components/card/CardTailwind';
 // import { ThemeProvider } from 'styled-components';
 // const theme = {
 //   colors: {
@@ -14,11 +15,7 @@ import CardTailwind from './components/card/CardTailwind';
 function App() {
   return (
     <div>
-      {/* <ThemeProvider theme={theme}></ThemeProvider> */}
-      {/* <GlobalStyles></GlobalStyles> */}
-      <CardList>
-        <CardTailwind primary={true}></CardTailwind>
-      </CardList>
+      <Photos></Photos>
     </div>
   );
 }
---
+++ src/components/photo/Photos.jsx
@@ -0,0 +1,12 @@
+import React, { useEffect } from 'react';
+
+const Photos = () => {
+  // useEffect(function callback() {
+  //     // side-effect
+  // }, []);
+  useEffect(() => {}, []);
+
+  return <div>Photos</div>;
+};
+
+export default Photos;
