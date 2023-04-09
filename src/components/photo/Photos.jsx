7eba158a1389db8352bc95aac9b7982951ff6fdb
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2023-04-09 15:31:19 +0700

    useEffect 3

--- src/components/photo/Photos.jsx
+++ src/components/photo/Photos.jsx
@@ -1,26 +1,28 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 // https://piscum.photos/v2/list
 
-const GetPhotos = () => {
+const getRandomPhotos = () => {
   return axios
-    .get('https://piscum.photos/v2/list')
+    .get('https://jsonplaceholder.typicode.com/photos')
     .then((res) => {
       console.log(res);
+      return res.data.slice(0, 5);
     })
     .catch((error) => console.log(error));
 };
 
 const Photos = () => {
-  // useEffect(function callback() {
-  //     // side-effect
-  // }, []);
+  const [randomPhotos, setRandomPhotos] = useState([]);
   useEffect(() => {
-    document.title = 'Welcome to useEffect';
+    getRandomPhotos().then((images) => {
+      console.log(images);
+      setRandomPhotos(images);
+    });
   }, []);
 
-  return <div>Photos</div>;
+  return <div>{JSON.stringify(randomPhotos)}</div>;
 };
 
 export default Photos;
