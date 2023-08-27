7b8a3664ef11852da693d635287f512058179371
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2023-08-27 14:32:52 +0700

    setup 2

---
+++ server.js
@@ -0,0 +1,11 @@
+const app = require('./src/app');
+
+const PORT = 5000;
+
+const server = app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+process.on('SIGINT', () => {
+  server.close(() => console.log(`Exit Server on port ${PORT}`));
+});
