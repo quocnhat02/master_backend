6953a3c757af126f43fb566c4a991950bfbc65fc
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2023-08-27 14:41:24 +0700

    morgan

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 const express = require('express');
+const morgan = require('morgan');
+
 const app = express();
 
 // init middlewares
+app.use(morgan('dev'));
 
 // init db
 
 // init routes
+app.get('/', (req, res, next) => {
+  res.status(500).send({
+    message: 'Hello',
+  });
+});
 
 // handling error
 
