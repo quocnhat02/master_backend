f889730409dd5c0fb92110b514ed4499c17b239f
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2023-08-27 23:46:35 +0700

    api sign up 10

--- src/services/access.service.js
+++ src/services/access.service.js
@@ -5,6 +5,7 @@ const bcrypt = require('bcrypt');
 const crypto = require('crypto');
 const KeyTokenService = require('./keyToken.service');
 const { createTokenPair } = require('../auth/authUtils');
+const { getInfoData } = require('../utils');
 
 const RolesShop = {
   SHOP: 'SHOP',
@@ -81,7 +82,10 @@ class AccessService {
         return {
           code: 201,
           metadata: {
-            shop: newShop,
+            shop: getInfoData({
+              fields: ['_id', 'name', 'email'],
+              object: newShop,
+            }),
             tokens,
           },
         };
