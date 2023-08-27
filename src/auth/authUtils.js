0dd854cdd7fcb8441c90211d5e20e231adbb5e61
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2023-08-27 23:57:16 +0700

    api sign up 12

--- src/auth/authUtils.js
+++ src/auth/authUtils.js
@@ -5,7 +5,7 @@ const JWT = require('jsonwebtoken');
 const createTokenPair = async (payload, publicKey, privateKey) => {
   try {
     // accessToken
-    const accessToken = await JWT.sign(payload, publicKey, {
+    const accessToken = await JWT.sign(payload, privateKey, {
       algorithm: 'RS256',
       expiresIn: '2 days',
     });
