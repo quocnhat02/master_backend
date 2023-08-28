102610f0a79e6aafbfc7f223f04709b61fd838e0
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2023-08-28 12:38:27 +0700

    success response 1

--- src/controllers/access.controller.js
+++ src/controllers/access.controller.js
@@ -4,6 +4,10 @@ const AccessService = require('../services/access.service');
 
 class AccessController {
   signUp = async (req, res, next) => {
+    // return res.status(201).json({
+    //   message: '',
+    //   metadata: ''
+    // })
     return res.status(201).json(await AccessService.signUp(req.body));
   };
 }
