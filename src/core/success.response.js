89109888e32b036c496f99d3e5fc937f12e61fd4
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2023-09-06 12:02:36 +0700

    response success

--- src/core/success.response.js
+++ src/core/success.response.js
@@ -1,8 +1,42 @@
 'use strict';
 
+const { ReasonPhrases, StatusCodes } = require('./httpStatusCode');
+
 class SuccessResponse extends Error {
-  constructor(message, status) {
-    super(message);
-    this.status = status;
+  constructor(
+    message,
+    statusCode = StatusCodes.OK,
+    reasonPhrases = ReasonPhrases.OK,
+    metadata = {}
+  ) {
+    this.message = !message ? reasonPhrases : message;
+    this.status = statusCode;
+    this.metadata = metadata;
+  }
+
+  send(res, headers = {}) {
+    return res.status(this.status).json(this);
   }
 }
+
+class OK extends SuccessResponse {
+  constructor(message, metadata = {}) {
+    super({ message, metadata });
+  }
+}
+
+class CREATED extends SuccessResponse {
+  constructor(
+    message,
+    statusCode = StatusCodes.CREATED,
+    reasonPhrases = ReasonPhrases.CREATED,
+    metadata = {}
+  ) {
+    super({ message, statusCode, reasonPhrases, metadata });
+  }
+}
+
+module.exports = {
+  OK,
+  CREATED,
+};
