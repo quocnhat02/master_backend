b3fa89e4e614fea31f19eaa70855b5053ec23ec6
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2023-11-02 01:56:03 +0700

    error response

--- src/services/access.service.js
+++ src/services/access.service.js
@@ -4,6 +4,7 @@ const crypto = require('crypto');
 const KeyTokenService = require('./keyToken.service');
 const { createTokenPair } = require('../auth/authUtils');
 const { getInfoData } = require('../utils');
+const { BadRequestError } = require('../core/error.response');
 
 const SALT_TO_HASH = 10;
 const ROLE_SHOP = {
@@ -15,78 +16,65 @@ const ROLE_SHOP = {
 
 class AccessService {
   static signUp = async ({ name, email, password }) => {
-    try {
-      const holderShop = await shopModel.findOne({ email }).lean();
+    const holderShop = await shopModel.findOne({ email }).lean();
 
-      if (holderShop) {
+    if (holderShop) {
+      throw new BadRequestError('Error: Shop already registered');
+    }
+
+    const hashPassword = await bcrypt.hash(password, SALT_TO_HASH);
+
+    const newShop = await shopModel.create({
+      name,
+      email,
+      password: hashPassword,
+      roles: [ROLE_SHOP.SHOP],
+    });
+
+    if (newShop) {
+      const privateKey = crypto.randomBytes(64).toString('hex');
+      const publicKey = crypto.randomBytes(64).toString('hex');
+
+      console.log({ privateKey, publicKey });
+
+      const keyStore = await KeyTokenService.createKeyToken({
+        userId: newShop._id,
+        publicKey,
+        privateKey,
+      });
+
+      if (!keyStore) {
         return {
           code: 'xxxx',
-          message: 'Shop already registered',
+          message: 'keyStore failed',
           status: 'error',
         };
       }
 
-      const hashPassword = await bcrypt.hash(password, SALT_TO_HASH);
+      const tokens = await createTokenPair(
+        { userId: newShop._id, email },
+        publicKey,
+        privateKey
+      );
 
-      const newShop = await shopModel.create({
-        name,
-        email,
-        password: hashPassword,
-        roles: [ROLE_SHOP.SHOP],
-      });
-
-      if (newShop) {
-        const privateKey = crypto.randomBytes(64).toString('hex');
-        const publicKey = crypto.randomBytes(64).toString('hex');
-
-        console.log({ privateKey, publicKey });
-
-        const keyStore = await KeyTokenService.createKeyToken({
-          userId: newShop._id,
-          publicKey,
-          privateKey,
-        });
-
-        if (!keyStore) {
-          return {
-            code: 'xxxx',
-            message: 'keyStore failed',
-            status: 'error',
-          };
-        }
-
-        const tokens = await createTokenPair(
-          { userId: newShop._id, email },
-          publicKey,
-          privateKey
-        );
-
-        console.log('Created Token success', tokens);
-
-        return {
-          code: 201,
-          metadata: {
-            shop: getInfoData({
-              fields: ['_id', 'name', 'email'],
-              object: newShop,
-            }),
-            tokens,
-          },
-        };
-      }
+      console.log('Created Token success', tokens);
 
       return {
-        code: 401,
-        metadata: null,
-      };
-    } catch (error) {
-      console.log(error);
-      return {
-        code: 'xxx',
-        message: error.message,
-        status: 'error',
+        code: 201,
+        metadata: {
+          shop: getInfoData({
+            fields: ['_id', 'name', 'email'],
+            object: newShop,
+          }),
+          tokens,
+        },
       };
     }
+
+    return {
+      code: 401,
+      metadata: null,
+    };
   };
 }
 
