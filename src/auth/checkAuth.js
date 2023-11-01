66e9ba53672410e66233d604f62f373354de2c9e
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2023-11-02 01:31:54 +0700

    apiKey

---
+++ src/auth/checkAuth.js
@@ -0,0 +1,56 @@
+const { findByIdApiKey } = require('../services/apiKey.service');
+
+const HEADER = {
+  API_KEY: 'x-api-key',
+  AUTHORIZATION: 'Authorization',
+};
+
+const apiKey = async (req, res, next) => {
+  try {
+    const key = req.headers[HEADER.API_KEY]?.toString();
+    if (!key) {
+      return res.status(403).json({
+        message: 'Forbidden error',
+      });
+    }
+
+    const objKey = await findByIdApiKey(key);
+    if (!objKey) {
+      return res.status(403).json({
+        message: 'Forbidden error',
+      });
+    }
+
+    req.objKey = objKey;
+    return next();
+  } catch (error) {
+    return res.status(403).json({
+      message: 'Forbidden error',
+    });
+  }
+};
+
+const permission = (permission) => {
+  return (req, res, next) => {
+    if (!req.objKey.permissions) {
+      return res.status(403).json({
+        message: 'Permission denied',
+      });
+    }
+
+    console.log('permission: ', req.objKey.permissions);
+    const validPermission = req.objKey.permissions.includes(permission);
+    if (!validPermission) {
+      return res.status(403).json({
+        message: 'Permission denied',
+      });
+    }
+
+    return next();
+  };
+};
+
+module.exports = {
+  apiKey,
+  permission,
+};
