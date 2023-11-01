d7c66f8de757be1e5477e5f46bb54490ef71e88b
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2023-11-01 17:46:51 +0700

    setup

---
+++ src/app.js
@@ -0,0 +1,27 @@
+// server
+const express = require('express');
+
+// middlewares
+const compression = require('compression');
+const { default: helmet } = require('helmet');
+const morgan = require('morgan');
+
+const app = express();
+
+// init middlewares
+app.use(morgan('dev'));
+app.use(helmet());
+app.use(compression());
+
+// init db
+
+// init routes
+app.get('/', (req, res, next) => {
+  res.status(200).json({
+    message: 'Hello',
+  });
+});
+
+// handle error
+
+module.exports = app;
